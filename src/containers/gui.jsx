a35eee9db261e55c6ee1aab4a5497d0bc4438ec1
aynonyaz <aynonyaz@gmail.com>
2026-09-24 22:14:16 +0000

    extensions thing

--- src/containers/gui.jsx
+++ src/containers/gui.jsx
@@ -21,7 +21,7 @@ import {
     closeCostumeLibrary,
     closeBackdropLibrary,
     closeTelemetryModal,
-    openExtensionLibrary,
+    openCustomExtensionModal,
 } from "../reducers/modals";
 
 import FontLoaderHOC from "../lib/font-loader-hoc.jsx";
@@ -190,7 +190,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    onExtensionButtonClick: () => dispatch(openExtensionLibrary()),
+    onExtensionButtonClick: () => dispatch(openCustomExtensionModal()),
     onActivateTab: (tab) => dispatch(activateTab(tab)),
     onActivateCostumesTab: () => dispatch(activateTab(COSTUMES_TAB_INDEX)),
     onActivateSoundsTab: () => dispatch(activateTab(SOUNDS_TAB_INDEX)),
